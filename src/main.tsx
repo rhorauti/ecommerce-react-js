f4dc2c624b566d57f867bde3cf14bf77a78b9095
rafael-horauti <rafael.horauti@uiclap.com>
2025-04-15 15:23:10 -0300

    teste

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 
-//
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
